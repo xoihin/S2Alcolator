f1509a10ae5865690a6d18ae37c214d78c1f1daa
Xoi Hin <xoihin@yahoo.com>
2015-09-19 14:49:20 -0400

    Checkpoint 21 completed, Basic Navigation

---
+++ SeAlcolator/SeAlcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  SeAlcolator
+//
+//  Created by Xoi Hin on 2015-09-19.
+//  Copyright (c) 2015 Xoi Hin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
